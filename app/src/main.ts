eb23a7ea84c3feb64941d9f50e233e6e8d728724
Shawn Duong <me@shawnd.xyz>
2025-05-18 18:40:41 -0700

    Revised bootstrapping files.

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,14 @@
+// Styling
 import "./assets/main.css";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+const vuetify = createVuetify({ components, directives });
+
+// Make the app.
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
@@ -7,8 +17,7 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
-
 app.use(createPinia());
 app.use(router);
-
+app.use(vuetify);
 app.mount("#app");
