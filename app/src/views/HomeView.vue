c8fff28d45e40a494ec2b1b59c542ac39416f8b2
Shawn Duong <me@shawnd.xyz>
2025-05-18 18:47:17 -0700

    Added a homepage file.

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<!-- ****************** Script ****************** -->
+<script setup lang="ts">
+</script>
+
+<!-- ****************** Template **************** -->
+<template>
+</template>
+
+<!-- ****************** Style ******************* -->
+<style scoped lang="scss">
+</style>
